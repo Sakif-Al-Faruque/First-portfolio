45f6feed919946d90af0d21218029452769ca218
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2022-02-24 18:40:02 +0600

    portfolio

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+//type js
+var typed = new Typed('.text-to-type', {
+    strings: ['Architechture', 'Animator', 'Developer'],
+    smartBackspace: true, // Default value
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  });
+
+//progress bar
+$('#pb-lan-1').LineProgressbar({
+      percentage: 90,
+      fillBackgroundColor:'#E63946',
+      backgroundColor:'#EDE6E3',
+      radius:'0px',
+      height:'10px',
+      width:'100%'
+});
+$('#pb-lan-2').LineProgressbar({
+    percentage: 95,
+    fillBackgroundColor:'#E63946',
+    backgroundColor:'#EDE6E3',
+    radius:'0px',
+    height:'10px',
+    width:'100%'
+});
+$('#pb-lan-3').LineProgressbar({
+    percentage: 60,
+    fillBackgroundColor:'#E63946',
+    backgroundColor:'#EDE6E3',
+    radius:'0px',
+    height:'10px',
+    width:'100%'
+});
+$('#pb-tech-1').LineProgressbar({
+    percentage: 90,
+    fillBackgroundColor:'#E63946',
+    backgroundColor:'#EDE6E3',
+    radius:'0px',
+    height:'10px',
+    width:'100%'
+});
+$('#pb-tech-2').LineProgressbar({
+  percentage: 95,
+  fillBackgroundColor:'#E63946',
+  backgroundColor:'#EDE6E3',
+  radius:'0px',
+  height:'10px',
+  width:'100%'
+});
+$('#pb-tech-3').LineProgressbar({
+  percentage: 60,
+  fillBackgroundColor:'#E63946',
+  backgroundColor:'#EDE6E3',
+  radius:'0px',
+  height:'10px',
+  width:'100%'
+});
+$('#pb-dep-1').LineProgressbar({
+    percentage: 90,
+    fillBackgroundColor:'#E63946',
+    backgroundColor:'#EDE6E3',
+    radius:'0px',
+    height:'10px',
+    width:'100%'
+});
